6a3160c47a5b1b189fddf3dd922d391eabb1314f
Nikita Dronov <nikoman007@gmail.com>
2020-04-05 14:08:57 +0400

    Add user_claim realtions to claim migration

--- database/migrations/2020_04_05_075300_create_claims_table.php
+++ database/migrations/2020_04_05_075300_create_claims_table.php
@@ -21,17 +21,28 @@ class CreateClaimsTable extends Migration
 
             $table->string('subject', 256);
             $table->text('body');
+            /**
+             * Three statusese by default:
+             * 1. O - open. Claim created but not assigned
+             * 2. P - processing. Claim assigned to manager
+             * 3. C - closed. Claim closed by manager or user
+             */
             $table->char('status', 1)->default('O');
-            $table->boolean('new_response');
             $table->timestamps();
 
             $table->foreign('user_id')->references('user_id')->on('users');
             $table->index('status');
         });
 
-        Schema::create('viewed_claims', function (Blueprint $table) {
-            $table->unsignedBigInteger('manager_id');
+        /**
+         * There are two relations type between users and claims:
+         * 1. V - determines if claim was viewed by specific manager
+         * 2. R - shows if claim has new responses for client\manager. 
+         */
+        Schema::create('claim_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('claim_id');
+            $table->char('relation_type', 1)->default('V');
         });
     }
 
