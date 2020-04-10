41aa6fb84fc972520da2e5a9c2f499a76f7020e0
Nikita Dronov <nikoman007@gmail.com>
2020-04-10 23:47:56 +0400

    CheckTime fix if no claims

--- app/Http/Middleware/CheckTime.php
+++ app/Http/Middleware/CheckTime.php
@@ -18,14 +18,17 @@ class CheckTime
     public function handle($request, Closure $next)
     {
         $last_claim = Claim::where('user_id', auth()->user()->user_id)->orderBy('created_at', 'desc')->first();
-        $new_claim_date = $last_claim->created_at->copy()->addDay();
-        $now = Carbon::now();
 
-        $diff = $new_claim_date->floatDiffInDays($now, false);
+        if (isset($last_claim)) {
+            $new_claim_date = $last_claim->created_at->copy()->addDay();
+            $now = Carbon::now();
 
-        if ($diff < 0) {
-            return redirect()->route('claims.index')
-                ->withErrors(['msg' => "Отправка нового заявления возможна через {$new_claim_date->diffInHours($now)}:{$new_claim_date->diff($now)->format('%I:%S')}"]);
+            $diff = $new_claim_date->floatDiffInDays($now, false);
+
+            if ($diff < 0) {
+                return redirect()->route('claims.index')
+                    ->withErrors(['msg' => "Отправка нового заявления возможна через {$new_claim_date->diffInHours($now)}:{$new_claim_date->diff($now)->format('%I:%S')}"]);
+            }
         }
 
         return $next($request);
